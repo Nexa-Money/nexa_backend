e73fb98a100b4d8a0708907b84f67ab74b4b8729
Felipe <feliperodriguesqueiroz122@gmail.com>
2025-05-22 10:09:09 -0300

    refactor: update StartServer to receive UserHandler instead of db connection

--- internal/service/service.go
+++ internal/service/service.go
@@ -5,10 +5,9 @@ import (
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
-	"github.com/jackc/pgx/v5"
 )
 
-func StartServer(conn *pgx.Conn) {
+func StartServer(uh *handler.UserHandler) {
 	app := fiber.New()
 
 	app.Use(cors.New(cors.Config{
@@ -16,7 +15,6 @@ func StartServer(conn *pgx.Conn) {
 		AllowMethods: "GET,POST,HEAD,PUT,DELETE,PATCH,OPTIONS",
 	}))
 
-	uh := handler.NewUserHandler(conn)
 	api := app.Group("/api/users")
 
 	api.Post("/", uh.CreateUser)
