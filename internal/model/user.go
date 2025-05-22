98151061b2e2d05e5b19c8c0d47ff48be79f0e03
Felipe <feliperodriguesqueiroz122@gmail.com>
2025-05-22 10:09:28 -0300

    fix: remove json omitempty tag to allow proper validation of empty fields

--- internal/model/user.go
+++ internal/model/user.go
@@ -8,9 +8,9 @@ import (
 
 type User struct {
 	ID        uuid.UUID `json:"id,omitempty" db:"id"`
-	Name      string    `json:"name,omitempty" db:"name"`
-	Email     string    `json:"email,omitempty" db:"email"`
-	Password  string    `json:"password,omitempty" db:"password"`
+	Name      string    `json:"name" db:"name"`
+	Email     string    `json:"email" db:"email"`
+	Password  string    `json:"password" db:"password"`
 	CreatedAt time.Time `json:"created_at,omitempty" db:"created_at"`
 	UpdatedAt time.Time `json:"updated_at,omitempty" db:"updated_at"`
 }
