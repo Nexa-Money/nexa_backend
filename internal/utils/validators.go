01a5b315c998db6545ac25111ea08e3410195d93
felipethecreator <feliperodriguesqueiroz122@gmail.com>
2025-05-29 21:06:56 -0300

    feat: add more validators

--- internal/utils/validators.go
+++ internal/utils/validators.go
@@ -1,6 +1,7 @@
 package utils
 
 import (
+	"fmt"
 	"nexa/internal/model"
 	"regexp"
 	"strings"
@@ -9,30 +10,55 @@ import (
 )
 
 func ValidateUser(c *fiber.Ctx, u model.User) error {
+	emailRegex := regexp.MustCompile(`^[a-zA-Z0-9.!#$%&'*+/=?^_` + "`" + `{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$`)
 
-	emailRegex := regexp.MustCompile("^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?(?:\\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?)*$")
+	if strings.TrimSpace(u.Name) == "" {
+		fmt.Println("NOME INVÁLIDO DETECTADO")
 
-	if strings.TrimSpace(u.Name) == "" || strings.TrimSpace(u.Email) == "" || strings.TrimSpace(u.Password) == "" {
 		return HttpError(c, ErrorStructure{
-			StatusCode: fiber.StatusBadRequest,
-			Message:    "Campos obrigatórios faltando",
-			Error:      "nome, email e senha são campos obrigatórios!",
+			StatusCode: 400,
+			Message:    "Campo nome obrigatório",
+			Error:      "O campo 'name' não pode estar vazio.",
 		})
 	}
 
-	if len(u.Password) < 6 {
+	if len(strings.TrimSpace(u.Name)) < 3 {
 		return HttpError(c, ErrorStructure{
-			StatusCode: fiber.StatusBadRequest,
-			Message:    "Campo senha incompleto",
-			Error:      "Sua senha deve ter pelo menos 6 dígitos!",
+			StatusCode: 400,
+			Message:    "Nome inválido",
+			Error:      "O nome deve ter pelo menos 3 caracteres.",
+		})
+	}
+
+	if strings.TrimSpace(u.Email) == "" {
+		return HttpError(c, ErrorStructure{
+			StatusCode: 400,
+			Message:    "Campo email obrigatório",
+			Error:      "O campo 'email' não pode estar vazio.",
 		})
 	}
 
 	if !emailRegex.MatchString(u.Email) {
 		return HttpError(c, ErrorStructure{
-			StatusCode: fiber.StatusBadRequest,
-			Message:    "Campo email incompleto",
-			Error:      "A estrutura do seu e-mail provavelmente está errada!",
+			StatusCode: 400,
+			Message:    "Email inválido",
+			Error:      "Formato de e-mail inválido.",
+		})
+	}
+
+	if strings.TrimSpace(u.Password) == "" {
+		return HttpError(c, ErrorStructure{
+			StatusCode: 400,
+			Message:    "Campo senha obrigatório",
+			Error:      "O campo 'password' não pode estar vazio.",
+		})
+	}
+
+	if len(u.Password) < 6 {
+		return HttpError(c, ErrorStructure{
+			StatusCode: 400,
+			Message:    "Senha fraca",
+			Error:      "A senha deve conter pelo menos 6 caracteres.",
 		})
 	}
 
