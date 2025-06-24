e3b364f7b80a2a9113ee64658ae309fc479903cc
Bruno César <bcgmeireles@gmail.com>
2025-06-24 08:51:34 -0300

    fix: solves railway env variables problem

--- internal/utils/load-env.go
+++ internal/utils/load-env.go
@@ -2,13 +2,16 @@ package utils
 
 import (
 	"log"
+	"os"
 
 	"github.com/joho/godotenv"
 )
 
 func LoadEnv() {
-	err := godotenv.Load()
-	if err != nil {
-		log.Printf("failed to load .env file: %v", err)
+	if os.Getenv("RAILWAY_ENVIRONMENT") == "" {
+		err := godotenv.Load()
+		if err != nil {
+			log.Printf("failed to load .env file: %v", err)
+		}
 	}
 }
