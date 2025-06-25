b4022202a60f7beb1576eb06a6253eb6937868c3
Marcos <marcos.lprado@sempreceub.com>
2025-06-25 15:06:47 -0300

    feat: Now transactions are totally associated with one user

--- internal/repository/transaction.go
+++ internal/repository/transaction.go
@@ -19,7 +19,7 @@ func NewTransactionRepository(conn *pgxpool.Pool) *TransactionRepository {
 }
 
 func (tr *TransactionRepository) InsertTransaction(transaction *model.Transaction) error {
-	query := `INSERT INTO "transaction" (id, user_id, category, amount, date, description, type, created_at)
+	query := `INSERT INTO "transaction" (id, user_id, category_id, amount, date, description, type, created_at)
 	          VALUES ($1, $2, $3, $4, $5, $6, $7, $8)`
 
 	_, err := tr.Conn.Exec(
@@ -35,7 +35,7 @@ func (tr *TransactionRepository) InsertTransaction(transaction *model.Transactio
 }
 
 func (tr *TransactionRepository) GetTransactions(id string) ([]model.Transaction, error) {
-	rows, err := tr.Conn.Query(context.Background(), `SELECT id, user_id, category, amount, date, description, type, created_at FROM "transaction" WHERE user_id = $1`, id)
+	rows, err := tr.Conn.Query(context.Background(), `SELECT id, user_id, category_id, amount, date, description, type, created_at FROM "transaction" WHERE user_id = $1`, id)
 	if err != nil {
 		return nil, err
 	}
